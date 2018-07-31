de5d9efe6884cedb40bc4c5afaf1d3ca51a9580d
Biven <biven@lougeek.com>
2018-07-31 10:28:29 -0400

    added details page and video loading on home page.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCimA4iJE3p7yZ3wZV6DXYNRIucry3VyAg';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create new Component and this component produce some html.
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos})
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        )
+    }
+
+}
+
+// Take this component html and put it on the page.
+ReactDom.render(<App/>, document.querySelector('.container'));
